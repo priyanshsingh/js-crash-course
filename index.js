4832175661c5abd4167349c56d177736a7ae0188
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-04 23:32:03 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 //JS Code
 console.log('Hemlo Bois Wassup!');
+
+//Variables
+let name = 'Priyansh Singh';
+console.log(name);
+
+let firstName = 'Varun';
+let lastName = 'Singh';
+console.log(firstName + ' ' +lastName);
+
+//Constants
+const x = 3;
+console.log(x);
+
+//Primitive Data types
