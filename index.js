5f56fe831a4c245c24c97154be7efed4f6f9a8fb
priyanshsingh <priyanshsingh1401@gmail.com>
2021-06-05 00:14:02 +0530

    modified index.js

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ const x = 3;
 console.log(x);
 
 //Primitive Data types
+let nameNew = 'Hans Zimmer' //String Literal
+let age = 19; //Number Literal
+let isApproved = true; //Boolean literal
+let new1 = undefined; //Undefined
+let new2 = null; //null
+
+//Objects
+let person = {
+    NAME : 'Priyansh',
+    AGE : 18 
+};
+
+//Dot Notation
+    person.NAME = 'Varun';
+    person.AGE = 19;
+console.log(person.NAME);
+
+//Bracket Notation
+    person['NAME'] = 'King';
+console.log(person.NAME);
+
