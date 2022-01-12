0ca2a9308a7045f371f7a7e3574492e2271929cb
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-13 00:09:01 +0530

    added js practice files

---
+++ quick_rev/p7.js
@@ -0,0 +1,14 @@
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors);
+
+selectedColors[2] = 'black';
+console.log(selectedColors);
+
+selectedColors[3] = 5;
+console.log(selectedColors);
+
+console.log(selectedColors[0]);
+
+console.log(typeof selectedColors);
+
+console.log(selectedColors.length)
---
+++ quick_rev/p8.js
@@ -0,0 +1,10 @@
+function greet(){
+    console.log("Hello,wassup?");
+}
+greet();
+
+function addnum(x, y){
+    console.log("Addition is =",(x+y));
+}
+
+addnum(5,10);
