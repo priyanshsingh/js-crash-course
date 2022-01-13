8d1ea4b47f051ea20b82bc2db3211b28958c7e10
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-13 18:38:31 +0530

    practice files were added

---
+++ quick_rev_cwh/index1.js
@@ -0,0 +1,19 @@
+// Number
+var number1 = 45;
+var number2 = 55;
+console.log(number1+number2);
+
+// Strings
+var str1 = 'Priyansh';
+var str2 = "Varun";
+console.log(str1, str2);
+
+// Object
+var objPerson = {
+    name : "Priyansh",
+    age : 19,
+    marks : 90
+};
+console.log(objPerson);
+console.log(objPerson.age);
+
