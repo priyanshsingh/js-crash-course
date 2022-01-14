647905bd523a62344480f90842b42f835706a8b7
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-14 18:41:05 +0530

    added data types in index1.html

---
+++ quick_rev_cwh/index2.js
@@ -0,0 +1,11 @@
+console.log("Something!");
+var a = 45;
+var b = 55;
+console.log("The value of a + b = ", a+b);
+console.log("The value of a - b = ", a-b);
+console.log("The value of a * b = ", a*b);
+console.log("The value of a / b = ", a/b);
+console.log("The value of a % b = ", a%b);
+
+var c = b;
+console.log(c);
